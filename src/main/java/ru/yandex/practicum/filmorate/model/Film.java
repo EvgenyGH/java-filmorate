5641582dd362af58157d72015430bdd03f76681d
Evgeny S <j3000@bk.ru>
2022-05-01 20:49:02 +0300

    feat: point1

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -8,23 +8,11 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
 @Slf4j
 public class Film {
-    public Film(int id, String name, String description, LocalDate releaseDate, Duration duration) {
-        validateReleaseDate(releaseDate);
-        validateDuration(duration);
-
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.releaseDate = releaseDate;
-        this.duration = duration;
-    }
-
     @Positive
     private int id;
     //название не может быть пустым
@@ -38,22 +26,19 @@ public class Film {
     @NotNull
     private LocalDate releaseDate;
     //продолжительность фильма должна быть положительной
-    @NotNull
-    private Duration duration;
+    @Positive
+    private int duration;
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        validateReleaseDate(releaseDate);
+        this.releaseDate = releaseDate;
+    }
 
     private void validateReleaseDate(LocalDate localDate) {
         if (localDate.isBefore(LocalDate.of(1895, 12, 28))) {
             log.warn(String.format("%-40s - %s", "Выброшено исключение"
                     , "Дата релиза должна быть не раньше 28 декабря 1895 года"));
             throw new ValidationException("Дата релиза должна быть не раньше 28 декабря 1895 года");
-        }
-    }
-
-    private void validateDuration(Duration duration) throws ValidationException {
-        if (duration.isNegative()) {
-            log.warn(String.format("%-40s - %s", "Выброшено исключение"
-                    , "Продолжительность фильма должна быть положительной"));
-            throw new ValidationException("Продолжительность фильма должна быть положительной");
         }
     }
 }
